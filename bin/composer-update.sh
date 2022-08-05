4a6c5ab645e0f1b0b7be3fa313e4b6a39cfe3c40
Dario Lamprecht <dario@lmprcht.de>
2022-08-05 10:33:59 +0200

    added a composer-update shell script (wip)

---
+++ bin/composer-update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Make sure we are able to access the forwarded ssh-auth-sock (store previous value)
+PERMISSIONS=$(stat -c "%a" "$SSH_AUTH_SOCK")
+sudo chmod 777 "${SSH_AUTH_SOCK}"
+
+# Pre-save known hosts
+ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts 2> /dev/null
+ssh-keyscan github.com >> ~/.ssh/known_hosts 2> /dev/null
+
+# Get project root (copied from bin/build.sh)
+BIN_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+export PROJECT_ROOT="${PROJECT_ROOT:-"$(dirname "$BIN_DIR")"}"
+
+# Do the update!
+composer update -d "${PROJECT_ROOT}" --no-scripts --no-interaction --no-install
+
+# Restore original permissions on ssh-auth-sock
+sudo chmod "$PERMISSIONS" "${SSH_AUTH_SOCK}"
